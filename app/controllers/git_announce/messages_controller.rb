b5e5067daf78ad47ef882ce6ef9a8c7687876560
Mike Schneider <mschneider3254@gmail.com>
2019-07-04 09:32:35 -0500

    More debugging

--- app/controllers/git_announce/messages_controller.rb
+++ app/controllers/git_announce/messages_controller.rb
@@ -8,24 +8,24 @@ module GitAnnounce
       
       editor      = request_payload['pull_request']['user']['login']
       link        = request_payload['pull_request']['url']
-      action_done = request_payload['pull_request']['action']
+      action_done = request_payload['action']
       repo_name   = request_payload['pull_request']['head']['repo']['name']
       number      = request_payload['pull_request']['number']
       
       
-      Rails.logger.debug 
-        "#{editor}, #{link}, #{action_done}, #{repo_name}, #{number}"
+      # Rails.logger.debug 
+      #   "#{editor}, #{link}, #{action_done}, #{repo_name}, #{number}"
       
-      # if action_done == "labeled"
-      #   full_message = "#{editor} added a label on pull request #{number} in [#{repo_name}](#{link})"
-      #   Http.zulip_message(repo_name, full_message)
+      if action_done == "labeled"
+        full_message = "#{editor} added a label on pull request #{number} in [#{repo_name}](#{link})"
+        Http.zulip_message(repo_name, full_message)
         
 
-      # elsif action_done == "unlabeled"
-      #   full_message = "#{editor} removed a label on pull request #{number} in [#{repo_name}](#{link})"
-      #   Http.zulip_message(repo_name, full_message)
+      elsif action_done == "unlabeled"
+        full_message = "#{editor} removed a label on pull request #{number} in [#{repo_name}](#{link})"
+        Http.zulip_message(repo_name, full_message)
         
-      # end
+      end
       
       head :ok
 
