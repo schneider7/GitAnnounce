d5b16c21c55e6abd62166cd9b035fbfed165aa19
Mike Schneider <mschneider3254@gmail.com>
2019-07-08 11:35:27 -0500

    Smol change

--- app/lib/git_announce/http.rb
+++ app/lib/git_announce/http.rb
@@ -6,9 +6,6 @@ module GitAnnounce
     def self.zulip_message(domain, stream_name, repo_name, content)
       uri = URI.parse("https://zulip.#{domain}.com/api/v1/messages")
       
-
-      #Bot email is github-updates-bot@zulip.sycamoreeducation.com
-      #Bot key is 2nwoBCfZHkghL6paI7j9PIY8th03K54T
       request = Net::HTTP::Post.new(uri) 
       request.basic_auth(ENV["BOT_EMAIL"], ENV["BOT_API_KEY"]) 
       request.body = "type=stream&to=#{stream_name}&subject=#{repo_name}&content=#{content}" 
