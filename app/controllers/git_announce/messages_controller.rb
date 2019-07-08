c992c6336a54f8589614ec4dcc5f46eaa063fcf4
Mike Schneider <mschneider3254@gmail.com>
2019-07-08 10:44:05 -0500

    Tweak message text

--- app/controllers/git_announce/messages_controller.rb
+++ app/controllers/git_announce/messages_controller.rb
@@ -8,8 +8,8 @@ module GitAnnounce
 
       action_done = request_payload['action'] 
       editor      = request_payload['pull_request']['user']['login']
-      number      = request_payload['number']
       repo_name   = request_payload['pull_request']['head']['repo']['name']
+      label       = request_payload['pull_request']['label']['name']
       title       = request_payload['pull_request']['title']
       link        = request_payload['pull_request']['_links']['html']['href']
          
@@ -17,11 +17,11 @@ module GitAnnounce
       #   "#{editor}, #{link}, #{action_done}, #{repo_name}, #{number}"
       
       if action_done == "labeled"
-        full_message = "#{editor} added a label on PR ##{number} in #{repo_name}. \n Link: [#{title}](#{link})"
+        full_message = "#{editor} added a '#{label}' label on [#{title}](#{link}) in #{repo_name}."
         Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)        
 
       elsif action_done == "unlabeled"
-        full_message = "#{editor} removed a label on PR ##{number} in #{repo_name}. Link: [#{title}](#{link})"
+        full_message = "#{editor} removed a '#{label}' label on [#{title}](#{link}) in #{repo_name}."
         Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)
         
       end
