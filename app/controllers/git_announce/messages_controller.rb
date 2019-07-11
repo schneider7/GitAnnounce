1e4cbd26014767c2a81f6e76e2c992ea7be63a34
Mike Schneider <mschneider3254@gmail.com>
2019-07-11 11:26:58 -0500

    Re-add switch, but only with one 'when' this time

--- app/controllers/git_announce/messages_controller.rb
+++ app/controllers/git_announce/messages_controller.rb
@@ -4,6 +4,7 @@ module GitAnnounce
     def receive
 
       request.body.rewind
+      event_type = request.headers["X-GitHub-Event"]
       request_payload = JSON.parse(request.body.read)
 
       action_done = request_payload['action']
@@ -13,26 +14,31 @@ module GitAnnounce
       title       = request_payload['pull_request']['title'] 
       link        = request_payload['pull_request']['_links']['html']['href']
 
-      if ['A','E','I','O','U'].include?(label[0]) # If first letter of label is a vowel
-        article = "an"
-      else
-        article = "a"
-      end
-         
-      if action_done == "labeled"
+      case event_type
+
+      when 'pull_request'
+
+        if ['A','E','I','O','U'].include?(label[0]) # If first letter of label is a vowel
+          article = "an"
+        else
+          article = "a"
+        end
+          
+        if action_done == "labeled"
+          
+          name = GitAnnounce.developers[owner.to_s.to_sym]
+          full_message = "@**#{name}**,  #{article} `#{label}` label was added to your PR:  [#{title}](#{link})."
+          Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)        
+
+        elsif action_done == "unlabeled"
+          name = GitAnnounce.developers[owner.to_s.to_sym]
+          full_message = "@**#{name}**,  #{article} `#{label}` label was removed from your PR:  [#{title}](#{link})."
+          Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)
+
+        end
         
-        name = GitAnnounce.developers[owner.to_s.to_sym]
-        full_message = "@**#{name}**,  #{article} `#{label}` label was added to your PR:  [#{title}](#{link})."
-        Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)        
-
-      elsif action_done == "unlabeled"
-        name = GitAnnounce.developers[owner.to_s.to_sym]
-        full_message = "@**#{name}**,  #{article} `#{label}` label was removed from your PR:  [#{title}](#{link})."
-        Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)
-
+        head :ok
       end
-      
-      head :ok
     end
   end
 end
