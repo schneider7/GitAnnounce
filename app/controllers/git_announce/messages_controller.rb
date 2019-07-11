841df534cab3f6f6818af37d6dd8d82f957c5941
Mike Schneider <mschneider3254@gmail.com>
2019-07-11 11:50:11 -0500

    Clean things up a little bit, working toward getting it back to where it was

--- app/controllers/git_announce/messages_controller.rb
+++ app/controllers/git_announce/messages_controller.rb
@@ -7,8 +7,6 @@ module GitAnnounce
       event_type = request.headers["X-GitHub-Event"]
       request_payload = JSON.parse(request.body.read)
 
-     
-
       case event_type
 
       when 'pull_request'
@@ -19,27 +17,27 @@ module GitAnnounce
         label       = request_payload['label']['name']
         title       = request_payload['pull_request']['title'] 
         link        = request_payload['pull_request']['_links']['html']['href']
+        sender      = request_payload['sender']['login']
 
         if ['A','E','I','O','U'].include?(label[0]) # If first letter of label is a vowel
           article = "an"
         else
           article = "a"
         end
-          
-        if action_done == "labeled"
-          
-          name = GitAnnounce.developers[owner.to_s.to_sym]
-          full_message = "@**#{name}**,  #{article} `#{label}` label was added to your PR:  [#{title}](#{link})."
-          Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)        
-
-        elsif action_done == "unlabeled"
-          name = GitAnnounce.developers[owner.to_s.to_sym]
-          full_message = "@**#{name}**,  #{article} `#{label}` label was removed from your PR:  [#{title}](#{link})."
-          Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)
-
-        end
         
-        head :ok
+        unless GitAnnounce.ignore.include?(sender)
+          name = GitAnnounce.developers[owner.to_s.to_sym]
+          
+          if action_done == "labeled"    
+            full_message = "@**#{name}**,  #{article} `#{label}` label was added to your PR:  [#{title}](#{link})."    
+
+          elsif action_done == "unlabeled"
+            full_message = "@**#{name}**,  #{article} `#{label}` label was removed from your PR:  [#{title}](#{link})."
+
+          end
+          Http.zulip_message(ENV["ZULIP_DOMAIN"], ENV["STREAM_NAME"], repo_name, full_message)
+          head :ok
+        end # unless
 
       when 'issue_comment'
         action_done = request_payload['action']
@@ -51,13 +49,32 @@ module GitAnnounce
         # If comment is made on PR
         if action_done == "created"
           name = GitAnnounce.developers[owner.to_s.to_sym]
-          full_message = "@**#{name}**, someone left a comment on your PR (`#{title}`). Read it [here](#{link})"
+          full_message = "@**#{name}**, someone left a comment on your PR '#{title}'. Read it [here](#{link})"
         end
 
         Http.zulip_message(ENV['ZULIP_DOMAIN'], ENV['STREAM_NAME'], repo_name, full_message)
         head :ok
-    
 
+      when 'pull_request_review'
+        action_done = payload['action']
+        status      = payload['review']['state']
+        owner       = payload['review']['user']['login']
+        link        = payload['review']['html_url']
+        title       = payload['pull_request']['title']     
+
+        name = GitAnnounce.developers[owner.to_s.to_sym]
+        
+        if action_done == "submitted"        
+          case status
+          when 'approved'
+            full_message = "@**#{name}**, a dev reviewer just approved changes on your PR (`#{title}`). Check it out [here](#{link})"
+          when 'changes_requested'
+            full_message = "@**#{name}**, changes were requested on your PR (`#{title}`). Read the comments [here](#{link})"
+          end
+        end
+
+        Http.zulip_message(ENV['ZULIP_DOMAIN'], ENV['STREAM_NAME'], repo_name, full_message)
+        head :ok
 
       end # when
     end # function
