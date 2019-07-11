364918888a63208a1efacf7db1c1ee41cfa3c56c
Mike Schneider <mschneider3254@gmail.com>
2019-07-11 11:00:33 -0500

    Continue to deconstruct code for debug

--- app/controllers/git_announce/messages_controller.rb
+++ app/controllers/git_announce/messages_controller.rb
@@ -11,125 +11,110 @@ module GitAnnounce
       case event_type       
       
       when 'pull_request'
-        repo_name, full_message = process_pull_request(payload)
-        # Handle pullrequest webhooks
+        # Getting values from parsed JSON that we'll need as arguments later
+        action_done   = payload['action']
+        owner         = payload['pull_request']['user']['login']
+        repo_name     = payload['pull_request']['head']['repo']['name']
+        label         = payload['label']['name']
+        title         = payload['pull_request']['title'] 
+        link          = payload['pull_request']['_links']['html']['href']
+        merged_status = payload['pull_request']['merged']
+        sender        = payload['sender']['login']
+
+        # Making sure message is grammatically correct
+        # if ['A','E','I','O','U'].include?(label[0]) # If first letter of label is a vowel
+        #   article = "an"
+        # else
+        #   article = "a"
+        # end 
+          
+        # unless GitAnnounce.ignore.include?(sender) # Ignore automatic changes by bots
+        # tab everything below once
+
+        # If label is added
+        if action_done == 'labeled'
+
+          # Special Case
+          if label.to_s == "!"
+            full_message = <<~MESSAGE
+              @**all** -- The `'!'` label was just added. 
+              [#{title}](#{link}) needs attention immediately.
+            MESSAGE
+
+          # Normal Case
+          else
+            full_message = "@**#{GitAnnounce.developers[name.to_s.to_sym]}**, #{article} `#{label}` label was added to your PR: [#{title}](#{link})."
+          end
+          
+        # If label is removed  
+        elsif action_done == 'unlabeled'
+            full_message = <<~MESSAGE
+              @**#{zulip_name(owner)}**, 
+              #{article} `#{label}` label was removed from your PR: 
+              [#{title}](#{link}).
+            MESSAGE
+          
+        # If PR is merged (into master)
+        elsif action_done == "closed" && merged_status == "true"
+          full_message = "@**#{GitAnnounce.developers[name.to_s.to_sym]}**, your PR [#{title}](#{link}) was just merged."
+        end    
+        # end
+
+        Http.zulip_message(ENV['ZULIP_DOMAIN'], ENV['STREAM_NAME'], repo_name, full_message)      
+        head :ok
+        
         
       when 'issue_comment'
-        repo_name, full_message = process_issue_comment(payload)
-        # Handle Comments webhooks
-        
-      when 'pull_request_review'
-        repo_name, full_message = process_pull_request_review(payload)
-        #Handle PR Review webhooks
-      end
+        action_done = payload['action']
+        owner       = payload['issue']['user']['login']
+        repo_name   = payload['repository']['name']
+        link        = payload['comment']['html_url']
+        title       = payload['issue']['title']
 
-      Http.zulip_message(ENV['ZULIP_DOMAIN'], ENV['STREAM_NAME'], repo_name, full_message)      
-      head :ok
-    end
-
-    private
-        def process_pull_request(payload)
-          # Getting values from parsed JSON that we'll need as arguments later
-          action_done   = payload['action']
-          owner         = payload['pull_request']['user']['login']
-          repo_name     = payload['pull_request']['head']['repo']['name']
-          label         = payload['label']['name']
-          title         = payload['pull_request']['title'] 
-          link          = payload['pull_request']['_links']['html']['href']
-          merged_status = payload['pull_request']['merged']
-          sender        = payload['sender']['login']
-
-          # Making sure message is grammatically correct
-          if ['A','E','I','O','U'].include?(label[0]) # If first letter of label is a vowel
-            article = "an"
-          else
-            article = "a"
-          end 
-            
-          unless GitAnnounce.ignore.include?(sender) # Ignore automatic changes by bots
-
-            # If label is added
-            if action_done == 'labeled'
-
-              # Special Case
-              if label == "!"
-                full_message = <<~MESSAGE
-                  @**all** -- The `'!'` label was just added. 
-                  [#{title}](#{link}) needs attention immediately.
-                MESSAGE
-
-              # Normal Case
-              else
-                full_message = <<~MESSAGE
-                  zulip_name(owner)}**, #{article} `#{label}` label was added to your PR:
-                  [#{title}](#{link}).
-                  MESSAGE
-              end
-              
-            # If label is removed  
-            elsif action_done == 'unlabeled'
-               full_message = <<~MESSAGE
-                  @**#{zulip_name(owner)}**, 
-                  #{article} `#{label}` label was removed from your PR: 
-                  [#{title}](#{link}).
-                MESSAGE
-              
-            # If PR is merged (into master)
-            elsif action_done == "closed" && merged_status == "true"
-              full_message = "@**#{zulip_name(owner)}**, your PR [#{title}](#{link}) was just merged."
-            end    
-          end
-          [repo_name, full_message]
+        # If comment is made on PR
+        if action_done == "created"
+          full_message = <<~MESSAGE
+            @**#{GitAnnounce.developers[name.to_s.to_sym]}**, someone left a comment on your PR (`#{title}`).
+            Read it [here](#{link})
+          MESSAGE
         end
 
-        def process_issue_comment(payload)
-          action_done = payload['action']
-          owner       = payload['issue']['user']['login']
-          repo_name   = payload['repository']['name']
-          link        = payload['comment']['html_url']
-          title       = payload['issue']['title']
+        Http.zulip_message(ENV['ZULIP_DOMAIN'], ENV['STREAM_NAME'], repo_name, full_message)      
+        head :ok
 
-          # If comment is made on PR
-          if action_done == "created"
+      
+      when 'pull_request_review'
+        action_done = payload['action']
+        status      = payload['review']['state']
+        owner       = payload['review']['user']['login']
+        link        = payload['review']['html_url']
+        title       = payload['pull_request']['title'] 
+        repo_name   = payload['pull_request']['head']['repo']['name']
+
+        if action_done == "submitted"        
+          case status
+          when 'approved'
             full_message = <<~MESSAGE
-              @**#{zulip_name(owner)}**, someone left a comment on your PR (`#{title}`).
-              Read it [here](#{link})
+              @**#{GitAnnounce.developers[name.to_s.to_sym]}**, a dev reviewer just approved changes on your PR 
+              (`#{title}`). Check it out [here](#{link})
+            MESSAGE
+
+          when 'changes_requested'
+            full_message = <<~MESSAGE
+              @**#{GitAnnounce.developers[name.to_s.to_sym]}**, changes were requested on your PR 
+              (`#{title}`). Read the comments [here](#{link})
             MESSAGE
           end
-
-          [repo_name, full_message]
         end
 
-        def process_pull_request_review(payload)
-          action_done = payload['action']
-          status      = payload['review']['state']
-          owner       = payload['review']['user']['login']
-          link        = payload['review']['html_url']
-          title       = payload['pull_request']['title'] 
-          repo_name   = payload['pull_request']['head']['repo']['name']
+        Http.zulip_message(ENV['ZULIP_DOMAIN'], ENV['STREAM_NAME'], repo_name, full_message)      
+        head :ok
+      end      
+    end
 
-          if action_done == "submitted"        
-            case status
-            when 'approved'
-              full_message = <<~MESSAGE
-                @**#{zulip_name(owner)}**, a dev reviewer just approved changes on your PR 
-                (`#{title}`). Check it out [here](#{link})
-              MESSAGE
+    # def zulip_name(name)
+    #     GitAnnounce.developers[name.to_s.to_sym]
+    # end
 
-            when 'changes_requested'
-              full_message = <<~MESSAGE
-                @**#{zulip_name(owner)}**, changes were requested on your PR 
-                (`#{title}`). Read the comments [here](#{link})
-              MESSAGE
-            end
-          end
-
-          [repo_name, full_message]
-        end
-
-        def zulip_name(name)
-          GitAnnounce.developers[name.to_s.to_sym]
-        end
-    
   end
 end
