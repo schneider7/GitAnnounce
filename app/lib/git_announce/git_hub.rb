ea56f3ce93559785312e013f4c3805131745b681
Mike Schneider <mschneider3254@gmail.com>
2019-07-12 11:46:06 -0500

    Update

--- app/lib/git_announce/git_hub.rb
+++ app/lib/git_announce/git_hub.rb
@@ -5,14 +5,14 @@ module GitAnnounce
 
     def self.get_comment_owner(username, repo_name, id)
 
+            
       uri = URI.parse("https://api.github.com/repos/#{username}/#{repo_name}/pulls/comments/#{id}")
-   
       response = Net::HTTP.get_response(uri)
-      
-      # payload = JSON.parse(response)
 
-      Rails.logger.debug response.inspect
-      # payload['user']['login']
+      payload = JSON.parse(response.body)
+      
+      puts payload
+      
     end
 
       
