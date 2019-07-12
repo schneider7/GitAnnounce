0da54086ba38e4756e7338ec350c17d95e2b1479
Mike Schneider <mschneider3254@gmail.com>
2019-07-12 11:35:55 -0500

    Update git_hub.rb

--- app/lib/git_announce/git_hub.rb
+++ app/lib/git_announce/git_hub.rb
@@ -6,12 +6,8 @@ module GitAnnounce
     def self.get_comment_owner(username, repo_name, id)
 
       uri = URI.parse("https://api.github.com/repos/#{username}/#{repo_name}/pulls/comments/#{id}")
-      request = Net::HTTP::Get.new(uri)
-
-      req_options = { use_ssl: uri.scheme == "https", }       
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request) 
-      end 
+   
+      response = Net::HTTP.get_response(uri)
       
       payload = JSON.parse(response.body.read)
 
