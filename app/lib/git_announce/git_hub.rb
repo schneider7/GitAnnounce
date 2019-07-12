618ec8642578ade99a6a46f7acbf7203fdd45f89
Mike Schneider <mschneider3254@gmail.com>
2019-07-12 14:47:59 -0500

    Begin adding auth to github api hit

--- app/lib/git_announce/git_hub.rb
+++ app/lib/git_announce/git_hub.rb
@@ -4,17 +4,12 @@ module GitAnnounce
   module GitHub
 
     def self.get_comment_owner(username, repo_name, id)
-
-            
       uri = URI.parse("https://api.github.com/repos/#{username}/#{repo_name}/pulls/comments/#{id}")
-      response = Net::HTTP.get_response(uri)
-
-      payload = JSON.parse(response.body)
+      request = Net::HTTP.Get.new(uri)
+      request["Authorization"] = "Authorization: token #{ENV["GITHUB_TOKEN"]}"
       
-      payload['user']['login']
-      
-    end
-
-      
+      payload = JSON.parse(response.body)      
+      payload['user']['login']      
+    end     
   end
 end
