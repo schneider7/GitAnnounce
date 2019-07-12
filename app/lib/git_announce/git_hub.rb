cf9373d3a8d6a792f8682c975c62b2878f72cd7b
Mike Schneider <mschneider3254@gmail.com>
2019-07-12 15:00:13 -0500

    Tweak git_hub.rb for testing

--- app/lib/git_announce/git_hub.rb
+++ app/lib/git_announce/git_hub.rb
@@ -3,13 +3,22 @@ require 'net/http'
 module GitAnnounce
   module GitHub
 
-    def self.get_comment_owner(username, repo_name, id)
-      uri = URI.parse("https://api.github.com/repos/#{username}/#{repo_name}/pulls/comments/#{id}")
-      request = Net::HTTP.Get.new(uri)
-      request["Authorization"] = "Authorization: token #{ENV["GITHUB_TOKEN"]}"
-      
-      payload = JSON.parse(response.body)      
-      payload['user']['login']      
+    def self.get_comment_owner(username, repo_name)
+      uri = URI.parse("https://api.github.com/repos/#{username}/#{repo_name}/comments")
+      request = Net::HTTP::Get.new(uri)
+      request["Authorization"] = "token #{ENV["GITHUB_TOKEN"]}"
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      payload = JSON.parse(response.body)     
+      payload 
+      # payload['user']['login']      
     end     
   end
 end
